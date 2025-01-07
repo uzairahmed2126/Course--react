5a242a61c6601df5abdc7089584114199abdd38a
uzairahmed2126 <uzairahmed21261@gmail.com>
2025-01-07 23:53:48 +0530

    hello world

---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+// components are useally  called functions and they are rendring in root file that is main file and the app file contains child components
+// Stateful = Logic + State Management + UI updates.
+// Stateless = UI Presentation with props only.
