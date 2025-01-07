ff2cb7f7f358e4a7e48272fd9d4c4e13de1c0d6f
uzairahmed2126 <uzairahmed21261@gmail.com>
2025-01-08 00:10:00 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-
+  const inputRef = useRef(null);
+  const handleBtn = (e) => {
+    // count++;
+    setCount((prev) => prev + 1);
+  };
   return (
     <>
-      <h1>Assalamu Alykum</h1>
+      <h1> {count}</h1>
+      <button onClick={handleBtn}>Click me</button>
     </>
   );
 }
 
 export default App;
+// const inputRef = useRef(null);
+
+// const focusInput = () => {
+//   inputRef.current.focus(); // Accesses and focuses the input element
+// };
+
+// return (
+//   <div>
+//     <input type="text" ref={inputRef} />
+//     <button onClick={focusInput}>Focus Input</button>
+//   </div>
+// );
